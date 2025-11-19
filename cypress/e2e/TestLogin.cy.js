a86b3323608883f36c96f0cab33ab216a04bb5e0
alexandrag <alexandrag@ENSRO0260.local>
2025-11-19 06:14:47 +0200

    Initial commit - Cypress first project

---
+++ cypress/e2e/TestLogin.cy.js
@@ -0,0 +1,16 @@
+describe('Login Test', () => {
+  it('Visits the login page and logs in', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+    // Enter username and password
+    cy.get('input[name="username"]').type('Admin')
+    cy.get('input[name="password"]').type('admin123')
+
+    // Click the login button
+    cy.get('button[type="submit"]').click()
+
+    // Verify that the dashboard has loaded
+    cy.url().should('include', '/dashboard')
+    cy.contains('Dashboard').should('be.visible')
+  })
+})
